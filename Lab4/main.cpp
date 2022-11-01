40cb3f0eaca395de11a9b05ec89d8ca7a2321af6
Kigris <kigrislord@gmail.com>
2022-11-01 11:32:01 +0100

    Initial Commit

---
+++ Lab4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab4
+//
+//  Created by Adrian Daniel Bodirlau on 01/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
